58f1653613ceec67fdc62f5014375040858a1859
Evan Meade <evan.meade@protonmail.com>
2020-08-17 23:18:26 -0500

    House session list scraper

--- config.py
+++ config.py
@@ -2,16 +2,21 @@ import os
 import configparser
 
 
-CONFIG_FILE = 'config.ini'
+CONFIG_FILE = os.path.expanduser('~/.texas-capital-view.ini')
 
 
 print('Make sure to run this script from the base directory of the repository!')
 
 config = configparser.ConfigParser()
 
-print('Writing base directory...')
-config['default'] = {'base_dir': str(os.getcwd())}
+print('Locating base directory...')
 
+chrome_dir = input('\nWhere is your ChromeDriver installed (for selenium)?\n')
+
+print('\nCompiling configuration...')
+config['default'] = {'base_dir': str(os.getcwd()), 'chrome_driver_dir': chrome_dir}
+
+print('\nWriting configuration...')
 with open(CONFIG_FILE, 'w') as cf:
     config.write(cf)
     cf.close()
--- scraping/house_scraper.py
+++ scraping/house_scraper.py
@@ -5,36 +5,50 @@ Representatives website.
 '''
 
 import re
+import os
 import pandas as pd
-import requests
 from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import configparser
 
 
 class HouseScraper:
     def __init__(self):
-        self.BASE_URL = 'https://journals.house.texas.gov/hjrnl'
+        self.BASE_URL = 'https://journals.house.texas.gov'
         self.QUERY_URL = 'https://journals.house.texas.gov/hjrnl/home.htm'
         self.MIN_YEAR = 82
-        self.url_list = pd.DataFrame(columns=['url', 'download_path'])
+        self.url_list = pd.DataFrame(columns=['url', 'download_path'], dtype='string')
+        self.url_list.index.name = 'session'
+
+        config = configparser.ConfigParser()
+        config.read(os.path.expanduser('~/.texas-capital-view.ini'))
+        self.base_dir = config['default']['base_dir']
+        self.chrome_driver_dir = config['default']['chrome_driver_dir']
+        self.options = Options()
+        self.options.headless = True
+
+        self.driver = webdriver.Chrome(options=self.options, executable_path=self.chrome_driver_dir)
 
     def query_years(self):
-        page = requests.get(self.QUERY_URL)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        self.driver.get(self.QUERY_URL)
+        soup = BeautifulSoup(self.driver.page_source, 'html.parser')
 
         menu = soup.find('select')
         sessions = menu.find_all('option')
 
         session_regex = r'(\d{2}[A-Z])|(\d{3})'
         for s in sessions:
-            match = re.search(session_regex, s.value)
+            print(s['value'])
+            match = re.search(session_regex, s['value'])
             if match:
-                year = int(match.group[:2])
+                year = int(match.group()[:2])
                 if year >= self.MIN_YEAR:
-                    self.url_list[match.group()] = [s.value, '']
+                    self.url_list.loc[match.group(), 'url'] = self.BASE_URL + s['value']
         
         print(self.url_list)
 
-
-hs = HouseScraper()
-hs.query_years()
-        
+        data_dir = os.path.join(self.base_dir, 'data')
+        if not os.path.isdir(data_dir):
+            os.mkdir(data_dir)
+        self.url_list.to_csv(os.path.join(data_dir, 'sessions.csv'))
