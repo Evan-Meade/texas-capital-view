2faddd37c932e1007903d8d877060a302f8937da
Evan Meade <evan.meade@protonmail.com>
2020-11-05 20:54:07 -0600

    Added re searches for reliability

--- txcapital/collect/house_member_scraper.py
+++ txcapital/collect/house_member_scraper.py
@@ -21,20 +21,31 @@ class HouseMemberScraper:
         member_divs = soup.find_all('td', class_='members-img-center')
 
         for div in member_divs:
+            member = {}
+
             id_num = uuid4().hex
+            member['_id'] = id_num
 
             member_name = div.find('strong').text.strip()
             split_name = member_name.split()
             first_name = split_name[2]
-            last_name = split_name[1][:-1]
-            middle_name = split_name[3] if len(split_name) > 3 else 'N/A'
-            full_name = f'{first_name} {middle_name} {last_name}' if middle_name != 'N/A' else f'{first_name} {last_name}'
+            last_name = re.search('[a-zA-Z]+', split_name[1])
+            name = {
+                'first': first_name,
+                'last': last_name
+            }
+            if len(split_name) > 3:
+                middle_name = split_name[3:]
+                if isinstance(middle_name, list):
+                    middle_name = ' '.join(middle_name)
+                name['middle'] = middle_name
+            full_name = f'{first_name} {middle_name} {last_name}' if 'middle' in name else f'{first_name} {last_name}'
+            name['full'] = full_name
 
-            district = int(div.text.strip().split()[1][:-4])
+            district_text = div.text
+            district = int(re.search('\d+', district_text).group())
 
-            
-
-            url_path = div.find('a')['href']
+            url_path = div.find('a').get('href')
             member_url = f'{HOUSE_BASE_URL}{url_path}'
 
             member = {
